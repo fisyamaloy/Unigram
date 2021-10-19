2f68098ec43f48323476e053f7549dd79782cb08
Danil Fisiuk <fisyuklol@gmail.com>
2021-10-20 02:51:12 +0300

    App abstraction and MainWindow implemented

---
+++ Client/Client.Qt/Source.cpp
@@ -0,0 +1,9 @@
+#include <Application.hpp>
+
+int main(int argc, char* argv[])
+{
+    Application* app = new Application(argc, argv);
+    app->create();
+    app->show();
+    return app->exec();
+}
